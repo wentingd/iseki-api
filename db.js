9f280d8eb99ff2030287db93d9ff09ccaed91668
wenting-duan <wenting.duan@axa.co.jp>
2019-06-18 20:07:13 +0900

    update outdated packages

--- db.js
+++ db.js
@@ -1,3 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/iseki');
+mongoose.connect(
+  'mongodb://localhost:27017/iseki',
+  { useNewUrlParser: true },
+);
